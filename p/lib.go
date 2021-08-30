bac6c54a0833ba0f1097e439849492d8cfa17deb
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2021-08-30 13:06:21 +0200

    Use version from build info

--- p/lib.go
+++ p/lib.go
@@ -1,10 +1,12 @@
 package p
 
 var (
-	name    = "b"
-	version = "v1.1.0"
-)
-
 func Version() string {
-	return name + ": " + version
+	bi, ok := debug.ReadBuildInfo()
+	if ok {
+		version := bi.Main.Version
+		version = strings.Trim(version, "()")
+		return fmt.Sprintf("%s@%s\n", bi.Main.Path, version)
+	}
+	return "unknown"
 }
